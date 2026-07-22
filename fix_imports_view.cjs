7d682d0afe56e1714d4cfb6a3ffe36442e3ee4d3
Alvin Guillermo <qajguillermo01@tip.edu.ph>
2026-07-22 17:23:14 +0800

    chore: remove obsolete import migration scripts
    Delete redundant scripts used for refactoring component import paths.

--- fix_imports_view.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/entry/ViewEntryModal.tsx', 'utf8');
-content = content.replace(/from '\.\.\/types'/g, "from '../../types'");
-content = content.replace(/from '\.\.\/lib\/utils'/g, "from '../../lib/utils'");
-content = content.replace(/from '\.\.\/db\/db'/g, "from '../../db/db'");
-content = content.replace(/from '\.\/ConfirmModal'/g, "from '../common/ConfirmModal'");
-fs.writeFileSync('src/components/entry/ViewEntryModal.tsx', content);
